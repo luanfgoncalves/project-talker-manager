90e0d0b8c46866ef64cb3ab250527903766c1568
luanfgoncalves <luanfgoncalves@outlook.com>
2022-11-04 14:37:31 -0300

    FIX: Corrige index.js para utilizar as rotas corretamente

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+// Requisições de módulos:
 const express = require('express');
 const bodyParser = require('body-parser');
+const router = require('./Routes/appRouter');
 
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
+const HTTP_OK_STATUS = 200;
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+// ------------------------------------------------------
+
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log('Online');
